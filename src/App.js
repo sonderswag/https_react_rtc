9c0dfac7bdc64c917b4dcd0fd402e13214b3ef76
sonderswag <csondergardwagner@gmail.com>
2018-06-16 08:45:27 -0700

    embedded agora rtc into react

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
 import React, { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
+const AgoraRTC = require('agora-rtc-sdk');
+
+AgoraRTC.Logger.error('this is error');
+AgoraRTC.Logger.warning('this is warning');
+AgoraRTC.Logger.info('this is info');
+AgoraRTC.Logger.debug('this is debug');
+
+var client, localStream, camera, microphone;
+
+
+function getDevices() {
+  AgoraRTC.getDevices(function (devices) {
+    for (var i = 0; i !== devices.length; ++i) {
+      // var device = devices[i];
+      // var option = document.createElement('option');
+      // option.value = device.deviceId;
+      // if (device.kind === 'audioinput') {
+      //   option.text = device.label || 'microphone ' + (audioSelect.length + 1);
+      //   audioSelect.appendChild(option);
+      // } else if (device.kind === 'videoinput') {
+      //   option.text = device.label || 'camera ' + (videoSelect.length + 1);
+      //   videoSelect.appendChild(option);
+      // } else {
+      //   console.log('Some other kind of source/device: ', device);
+      // }
+      console.log(devices[i]);
+    }
+  });
+};
 
 class App extends Component {
+
   render() {
+    if(!AgoraRTC.checkSystemRequirements()) {
+      alert("browser is no support webRTC");
+    }
+    getDevices();
+    
+
     return (
       <div className="App">
         <header className="App-header">
